3fd1c11ec68bf688e2bf43460598708255325924
Broteen Das <broteendas.official@gmail.com>
2024-05-18 13:01:59 +0530

    Create pulse.py

---
+++ pulse.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+drone = Drone(connection_string='127.0.0.1:14550') # Connects to connection_string
+
+print("Takeoff!")
+drone.arm_and_takeoff(5)
+sleep(1)
+print("RTL!")
+drone.rtl()
+
+drone.close_vehicle()
+
+print("Closed!")
